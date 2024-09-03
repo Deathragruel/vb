3330de982f05a71bc97bc10b28138498e82eb9e3
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 18:18:46 +0500

    complete

---
+++ selection/state_of_water/state_of_water/Module1.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim temperature As Double
+
+        WriteLine("Enter temperature of water in celcius without units.")
+
+        temperature = ReadLine()
+
+        If temperature = 0 Then
+            WriteLine("The water is freezing.")
+        ElseIf temperature = 100 Then
+            WriteLine("The water is boiling.")
+        ElseIf temperature < 100 And temperature > 0 Then
+            WriteLine("The water is not boiling or freezing.")
+        Else
+            WriteLine("That's not water...")
+        End If
+
+        ReadKey()
+    End Sub
+
+End Module
---
+++ selection/state_of_water/state_of_water/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("state_of_water")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("state_of_water")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("36124e44-24c9-4d7c-94ee-f6f1aea854e5")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
