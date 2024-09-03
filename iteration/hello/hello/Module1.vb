0acbe7c0f3ae2dafacc8be199c36a8c9e2f930ac
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 19:18:25 +0500

    complete

---
+++ iteration/hello/hello/Module1.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim counter As Integer
+
+        For counter = 1 To 4
+            Write("Hello ")
+        Next
+
+        ReadKey()
+    End Sub
+
+End Module
