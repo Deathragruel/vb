dbccc04fb3b46c76a310fb8f08cfd9b72f8110f6
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 14:33:41 +0500

    complete

---
+++ volume/volume/Module1.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim length, width, depth As Double
+
+        Write("Enter the length, width, and depth of a rectangular swimming pool ")
+        WriteLine("to calculate its volume.")
+
+        length = ReadLine()
+        width = ReadLine()
+        depth = ReadLine()
+
+        WriteLine("Volume: " & (length * width * depth))
+
+        ReadKey()
+    End Sub
+
+End Module
