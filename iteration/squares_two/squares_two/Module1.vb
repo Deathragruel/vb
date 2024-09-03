c09113ca40c73510b33b9e8cb7bc73f6aa6dabfe
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 19:50:31 +0500

    complete

---
+++ iteration/squares_two/squares_two/Module1.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim index As Integer
+
+        WriteLine("Number  Square of Number")
+
+        For index = 1 To 12
+            If index < 10 Then
+                WriteLine(index & "             " & index ^ 2)
+            Else
+                WriteLine(index & "            " & index ^ 2)
+            End If
+        Next
+
+        ReadKey()
+    End Sub
+
+End Module
