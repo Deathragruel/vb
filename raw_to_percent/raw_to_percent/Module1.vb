8e72ce43186912a72d7c9699fc60ac3a37a16dcd
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 15:45:00 +0500

    complete

---
+++ raw_to_percent/raw_to_percent/Module1.vb
@@ -0,0 +1,17 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim attained_marks, total_marks As Integer
+        Dim raw_marks As Double
+
+        WriteLine("Enter attained marks and total marks one by one.")
+        attained_marks = ReadLine()
+        total_marks = ReadLine()
+        raw_marks = attained_marks / total_marks
+        WriteLine("Percentage: " & (raw_marks * 100).ToString("F2") & "%")
+
+        ReadKey()
+    End Sub
+
+End Module
---
+++ raw_to_percent/raw_to_percent/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("raw_to_percent")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("raw_to_percent")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("cbdefa4c-37c7-4cb1-a04b-0c1282c8394e")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
