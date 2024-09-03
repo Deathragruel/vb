362349041dc061b73c0f48f774bc9f616f7e4962
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 16:27:42 +0500

    complete

---
+++ pound_to_euro/pound_to_euro/Module1.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Console
+
+Module Module1
+
+    Sub Main()
+        Dim pounds, exchange_rate As Double
+
+        WriteLine("Enter an amount of money in pounds for conversion to euros.")
+        WriteLine("Afterward, enter the exchange rate you wish to use. (Standard is 1.15)")
+
+        pounds = ReadLine()
+        exchange_rate = ReadLine()
+
+        WriteLine("Converted: " & (exchange_rate * pounds) & ChrW(&HA3))
+
+        ReadKey()
+    End Sub
+
+End Module
---
+++ pound_to_euro/pound_to_euro/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("pound_to_euro")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("pound_to_euro")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5f64b9a3-1b8c-4c8f-8d9a-24d588b548bb")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
