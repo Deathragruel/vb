2bf02cd9d23fd98f70335188719dcca8d9a765a6
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 19:27:41 +0500

    complete

---
+++ iteration/message/message/Module1.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim count, index As Integer
+        Dim message As String
+
+        WriteLine("Enter a message, and the amount of times it is to be repeated")
+
+        message = ReadLine()
+        count = ReadLine()
+
+        For index = 1 To count
+            WriteLine(message)
+        Next
+
+        ReadKey()
+    End Sub
+
+End Module
