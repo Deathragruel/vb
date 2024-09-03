66d68a212884e3594820334786ea1aa841c18fa2
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 20:53:26 +0500

    complete

---
+++ iteration/binary/binary/Module1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim num, result As Integer
+        Dim binary As String
+
+        WriteLine("Enter a number you wish to convert into binary.")
+
+        num = ReadLine()
+
+        If num = 0 Then
+            WriteLine("0")
+            Exit Sub
+        End If
+
+        binary = ""
+        result = num
+        While result > 0
+            binary = binary & (result Mod 2)
+            result = result \ 2
+        End While
+
+        WriteLine(StrReverse(binary))
+
+        ReadKey()
+    End Sub
+
+End Module
---
+++ iteration/factorial/factorial/Module1.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim index, num, total As Integer
+
+        WriteLine("Enter n such that you wish to calculate n!, where n is a + integer.")
+
+        num = ReadLine()
+
+        total = 1
+        For index = 1 To num
+            total = total * index
+        Next
+
+        If num > 0 Then
+            WriteLine(num & "! = " & total)
+        ElseIf num = 0 Then
+            WriteLine("0! = 1")
+        Else
+            WriteLine("No value of n! for n<0.")
+        End If
+
+
+        ReadKey()
+    End Sub
+
+End Module
