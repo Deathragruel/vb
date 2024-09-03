5319d5facb715681aaf5bcf24851ad5eb1ecb490
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 21:09:57 +0500

    complete

---
+++ iteration/add/add/Module1.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Console
+Imports System.Linq.Expressions
+Module Module1
+
+    Sub Main()
+        Dim num, total, counter As Double
+
+        WriteLine("Enter numbers infinitely till you don't want to, then enter 0.")
+
+        total = 0
+        counter = 0
+        Do
+            num = ReadLine()
+            total += num
+            counter += 1
+        Loop Until num = 0
+        counter -= 1
+
+        WriteLine("Total: " & total)
+        WriteLine("Average: " & (total / counter).ToString("F2"))
+
+        ReadKey()
+    End Sub
+
+End Module
