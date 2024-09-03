b4f7ea8d9179a50c69aab2dc533931214a1eee52
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 13:35:37 +0500

    complete

---
+++ sum/sum/Module1.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim num1, num2, num3 As Integer
+
+        WriteLine("Enter three integers one by one to add them all together.")
+        num1 = ReadLine()
+        num2 = ReadLine()
+        num3 = ReadLine()
+        WriteLine("Solution: " & (num1 + num2 + num3))
+
+        ReadKey()
+    End Sub
+
+End Module
