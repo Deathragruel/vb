7e999c1718f3bcf07df253f7e8274e79230aab39
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 19:32:13 +0500

    complete

---
+++ iteration/eight_times_table/eight_times_table/Module1.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim index As Integer
+
+        For index = 1 To 10
+            WriteLine("8 multipled by " & index & " = " & (8 * index))
+        Next
+
+        ReadKey()
+    End Sub
+
+End Module
