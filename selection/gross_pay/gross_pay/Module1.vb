2fc68d9919157c5cc30a215fa3434bab0f5f4bc1
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 18:32:17 +0500

    complete

---
+++ selection/gross_pay/gross_pay/Module1.vb
@@ -0,0 +1,25 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim hours_worked, hourly_pay, gross_pay As Double
+
+        WriteLine("Enter hours worked this week and hourly pay to calculate gross pay.")
+
+        hours_worked = ReadLine()
+        hourly_pay = ReadLine()
+
+        If hours_worked >= 0 And hours_worked <= 40 Then
+            gross_pay = (hours_worked * hourly_pay).ToString("F2")
+            WriteLine("Gross pay: " & gross_pay)
+        ElseIf hours_worked > 40 And hours_worked <= 60 Then
+            gross_pay = ((40 * hourly_pay) + ((hours_worked - 40) * hourly_pay * 1.5)).ToString("F2")
+            WriteLine("Gross pay: " & gross_pay)
+        Else
+            WriteLine("Invalid(0<=hours_worked<=60)")
+        End If
+
+        ReadKey()
+    End Sub
+
+End Module
---
+++ selection/gross_pay/gross_pay/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("gross_pay")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("gross_pay")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1606f5c5-d649-4592-a155-3aa3658e566b")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
