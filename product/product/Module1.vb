ff030006613f1f288669efd511c295b28a378a61
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 14:28:01 +0500

    complete

---
+++ product/product/Module1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim num1, num2 As Integer
+
+        WriteLine("Enter two integers for multiplication.")
+        num1 = ReadLine()
+        num2 = ReadLine()
+        WriteLine("Solution: " & (num1 * num2))
+
+        ReadKey()
+    End Sub
+
+End Module
