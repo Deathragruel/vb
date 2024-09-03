6410ac01f88a37d144b335ff9503e562603c7989
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 19:58:02 +0500

    complete

---
+++ iteration/test_even_10000/test_even_10000/Module1.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim index As Integer
+
+        For index = 2 To 10000 Step 2
+            WriteLine(index)
+        Next
+
+        ReadKey()
+    End Sub
+
+End Module
