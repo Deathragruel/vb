2fa10ea9aea7749262f7cf2677d12a7a893e3fcc
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 18:04:21 +0500

    complete

---
+++ selection/larger_for_three/larger_for_three/Module1.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim num1, num2, num3 As Double
+
+        WriteLine("Enter three numbers to determine which is largest.")
+
+        num1 = ReadLine()
+        num2 = ReadLine()
+        num3 = ReadLine()
+
+        If num1 > num2 And num1 > num3 Then
+            WriteLine(num1 & " is the largest.")
+        ElseIf num2 > num1 And num2 > num3 Then
+            WriteLine(num2 & " is the largest.")
+        ElseIf num3 > num1 And num3 > num2 Then
+            WriteLine(num3 & " is the largest.")
+        Else
+            WriteLine("All three are equal.")
+        End If
+
+        ReadKey()
+
+    End Sub
+
+End Module
---
+++ selection/larger_for_three/larger_for_three/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("larger_for_three")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("larger_for_three")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("07006d94-96aa-4c41-8529-e4e07f0f93e1")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
