e923482ed3ad1503f842d1aea7d2862ad4c08e63
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 15:19:50 +0500

    complete

---
+++ area/area/Module1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim length, breadth As Double
+
+        WriteLine("Enter length and breadth of a rectangle to output area.")
+        length = ReadLine()
+        breadth = ReadLine()
+        WriteLine("Area: " & (length * breadth))
+
+        ReadKey()
+    End Sub
+
+End Module
