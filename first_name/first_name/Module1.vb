e4e0377c7a07290bcc8aae69bd4bbb23fd931c1f
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 15:07:48 +0500

    complete

---
+++ first_name/first_name/Module1.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim name As String
+
+        WriteLine("What is your name?")
+        name = ReadLine()
+        WriteLine("Hello " & name & ", how's life nowadays?")
+
+        ReadKey()
+    End Sub
+
+End Module
