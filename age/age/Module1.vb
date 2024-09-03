4b14a4ea4effd95cdc759b1fd223b1d57c9670e5
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 17:39:59 +0500

    complete

---
+++ age/age/Module1.vb
@@ -0,0 +1,18 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim age As String
+
+        WriteLine("What is your age?")
+        age = ReadLine()
+
+        If age >= 18 Then
+            WriteLine("You are old enough to drive.")
+        Else
+            WriteLine("You are not old enough to drive.")
+        End If
+
+    End Sub
+
+End Module
