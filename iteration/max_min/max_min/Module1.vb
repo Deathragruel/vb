557911953ec2e106050e996d5567f05635547e36
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 19:56:31 +0500

    complete

---
+++ iteration/max_min/max_min/Module1.vb
@@ -0,0 +1,14 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim index As Integer
+
+        For index = 1 To 10000
+            WriteLine(index)
+        Next
+
+        ReadKey()
+    End Sub
+
+End Module
---
+++ iteration/max_min/max_min/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("max_min")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("max_min")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c75d3bee-5c34-4dc0-bdea-d6c0ef55b12a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
