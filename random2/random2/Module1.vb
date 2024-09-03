6c2700e974254a12283ca58c7d8803f4b1ca4050
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 15:04:45 +0500

    complete

---
+++ random2/random2/Module1.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim index, random_num As Integer
+
+        For index = 1 To 6
+            Randomize()
+            random_num = (Rnd() * 6) + 5
+            WriteLine(random_num)
+        Next
+
+        ReadKey()
+    End Sub
+
+End Module
