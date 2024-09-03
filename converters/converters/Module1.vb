2e231eca61ee5a2b6cb9bb00567475925f4b7923
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 17:10:00 +0500

    complete

---
+++ converters/converters/Module1.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim celsius, farenheit, inches, stones As Double
+
+        WriteLine("Enter temperature in celsius to convert to farenheit.")
+
+        celsius = ReadLine()
+        farenheit = (((9 / 5) * celsius) + 32).ToString("F1")
+
+        WriteLine("Farenheit: " & farenheit)
+
+        WriteLine("")
+        WriteLine("Enter inches to convert to centimeter.")
+
+        inches = ReadLine()
+
+        WriteLine("Centimeters: " & (2.54 * inches).ToString("F0"))
+
+        WriteLine("")
+        WriteLine("Enter stones to convert to kilograms.")
+
+        stones = ReadLine()
+
+        WriteLine("Kilograms: " & (6.364 * stones).ToString("F1"))
+
+        ReadKey()
+    End Sub
+
+End Module
