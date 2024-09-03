e5030d2b999358a2f7487bc66c845724380f2642
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 15:26:26 +0500

    complete

---
+++ circle_info/circle_info/Module1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim radius As Double
+
+        WriteLine("Enter radius of a circle for circumference and area.")
+        radius = ReadLine()
+        WriteLine("Circumferemnce: " & Format(2 * Math.PI * radius, "##########.##"))
+        WriteLine("Area: " & Format((Math.PI ^ 2) * radius, "##########.##"))
+
+        ReadKey()
+    End Sub
+
+End Module
