7a1baa31a78fdda91b371b1d1e5b8b0c9d457593
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 20:14:21 +0500

    complete

---
+++ iteration/max_min/max_min/Module1.vb
@@ -0,0 +1,29 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim index As Integer
+        Dim num, max, min As Double
+
+        WriteLine("Enter 10 numbers for attaining maximum and minimum values.")
+
+        num = ReadLine()
+        max = num
+        min = num
+        For index = 1 To 9
+            num = ReadLine()
+            If num > max Then
+                max = num
+            ElseIf num < min Then
+                min = num
+            End If
+        Next
+
+        WriteLine("")
+        WriteLine("Max: " & max)
+        WriteLine("Min: " & min)
+
+        ReadKey()
+    End Sub
+
+End Module
