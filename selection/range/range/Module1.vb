30aafe41a04ce8aef930f01cbd683ff74efa9676
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 17:57:00 +0500

    complete

---
+++ selection/range/range/Module1.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim num As Integer
+
+        WriteLine("Enter number to check if its between 21 and 29 inclusive.")
+
+        num = ReadLine()
+
+        If num >= 21 And num <= 29 Then
+            WriteLine("Within range")
+        Else
+            WriteLine("Outside range")
+        End If
+
+        ReadKey()
+    End Sub
+
+End Module
