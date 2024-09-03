c31d428062bd7ac94b1ba80f873a2b0fbe8047db
rafehy bin asif <rafehy6@gmail.com>
2024-09-03 15:16:09 +0500

    complete

---
+++ decimal_product/decimal_product/Module1.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim num1, num2, result As Double
+
+        WriteLine("Enter two real numbers one by one for multiplication rounded to two dp.")
+        num1 = ReadLine()
+        num2 = ReadLine()
+        result = num1 * num2
+        WriteLine("Here's your answer: " & Format(result, "#########.##"))
+
+        ReadKey()
+    End Sub
+
+End Module
