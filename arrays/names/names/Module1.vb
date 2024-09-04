8d8a8fd153c7431e215ce17ada4af93191632434
rafehy bin asif <rafehy6@gmail.com>
2024-09-04 20:01:13 +0500

    complete

---
+++ arrays/names/names/Module1.vb
@@ -0,0 +1,30 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim index As Integer
+        Dim names(5) As String
+
+        WriteLine("Enter 6 names.")
+
+        For index = 0 To 5
+            names(index) = ReadLine()
+        Next
+
+        WriteLine("")
+
+        For index = 0 To 5
+            WriteLine(names(index))
+        Next
+
+        WriteLine("")
+
+        For index = 5 To 0 Step -1
+            WriteLine(names(index))
+        Next
+
+
+        ReadKey()
+    End Sub
+
+End Module
---
+++ iteration/game/game/Module1.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim random_num, guessing_num As Integer
+
+        Randomize()
+        random_num = (1000 * Rnd()) + 1
+
+        WriteLine("Keep guessing till you get the right number from 1 to 1000.")
+
+        Do
+            guessing_num = ReadLine()
+
+            If guessing_num < random_num Then
+                WriteLine("Too low.")
+            ElseIf guessing_num > random_num Then
+                WriteLine("Too high.")
+            End If
+
+        Loop Until guessing_num = random_num
+
+        WriteLine("You win!")
+
+        ReadKey()
+    End Sub
+
+End Module
