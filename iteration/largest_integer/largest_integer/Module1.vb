b0ab07dcf263566d0157b9acce0164dab27ae1a4
rafehy bin asif <rafehy6@gmail.com>
2024-09-04 18:38:53 +0500

    complete

---
+++ iteration/largest_integer/largest_integer/Module1.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim index, num, max, min As Integer
+
+        WriteLine("Enter 8 integers one by one.")
+
+        num = ReadLine()
+        max = num
+        min = num
+        For index = 1 To 7
+            num = ReadLine()
+            If num > max Then
+                max = num
+            ElseIf num < min Then
+                min = num
+            End If
+        Next
+        WriteLine("Max: " & max)
+        WriteLine("Min: " & min)
+
+        ReadKey()
+    End Sub
+
+End Module
