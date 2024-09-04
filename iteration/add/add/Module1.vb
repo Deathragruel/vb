daec749daa86a1e8195f2ce8b234d6c3b3c75d1d
rafehy bin asif <rafehy6@gmail.com>
2024-09-04 17:14:54 +0500

    updated

--- iteration/add/add/Module1.vb
+++ iteration/add/add/Module1.vb
@@ -7,13 +7,12 @@ Module Module1
         WriteLine("Enter numbers infinitely till you don't want to, then enter 0.")
 
         total = 0
-        counter = 0
+        counter = -1
         Do
             num = ReadLine()
             total += num
             counter += 1
         Loop Until num = 0
-        counter -= 1
 
         WriteLine("Total: " & total)
         WriteLine("Average: " & (total / counter).ToString("F2"))
