090bd4b17d9892914229baf10c6a619822a09263
rafehy bin asif <rafehy6@gmail.com>
2024-09-04 18:25:53 +0500

    complete

---
+++ iteration/conversion_table/conversion_table/Module1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim pounds As Double
+
+        WriteLine("Pounds   Kilograms")
+        For pounds = 1 To 20
+            WriteLine("  " & pounds & "          " & (pounds / 2.2).ToString("F2"))
+        Next
+
+        ReadKey()
+    End Sub
+
+End Module
---
+++ iteration/conversion_table/conversion_table/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("conversion_table")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("conversion_table")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("7c74d30e-ecb5-4c5a-8cbf-8a40a80a7d9c")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
