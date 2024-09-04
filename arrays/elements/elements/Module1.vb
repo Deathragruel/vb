b9f3a5dbccc5df14a4eadbe6c6b276383657b9d4
rafehy bin asif <rafehy6@gmail.com>
2024-09-04 20:04:09 +0500

    complete

---
+++ arrays/elements/elements/Module1.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim index, elements(4) As Integer
+
+        WriteLine("Enter 5 elements for display.")
+
+        For index = 0 To 4
+            elements(index) = ReadLine()
+        Next
+        WriteLine("")
+        For index = 0 To 4
+            WriteLine(elements(index))
+        Next
+
+        ReadKey()
+    End Sub
+
+End Module
