4b94cc0085e3d8c04b873f8a2866116d3498f28f
rafehy bin asif <rafehy6@gmail.com>
2024-09-04 20:18:56 +0500

    complete

---
+++ arrays/max_and_index/max_and_index/Module1.vb
@@ -0,0 +1,31 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim amount, index, max, max_index As Integer
+
+        WriteLine("How many values do you wish to enter?")
+        amount = ReadLine()
+
+        Dim values(amount - 1) As Integer
+
+        WriteLine("Enter " & amount & " values.")
+
+        values(0) = ReadLine()
+        max = values(0)
+
+        For index = 1 To (amount - 1)
+            values(index) = ReadLine()
+            If values(index) > max Then
+                max = values(index)
+                max_index = index
+            End If
+        Next
+
+        WriteLine("Max value entered: " & max)
+        WriteLine("Index location in list: " & max_index)
+
+        ReadKey()
+    End Sub
+
+End Module
