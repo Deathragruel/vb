85d4d3b7125dfba9676cf78074d8b02b51eef9f5
rafehy bin asif <rafehy6@gmail.com>
2024-09-04 17:27:45 +0500

    complete

---
+++ iteration/range_validation/range_validation/Module1.vb
@@ -0,0 +1,15 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        Dim num As Integer
+
+        Do
+            WriteLine("Please enter a number between 10 and 20 inclusive or 0 to exit.")
+            num = ReadLine()
+
+        Loop Until num >= 10 And num <= 20 Or num = 0
+
+    End Sub
+
+End Module
