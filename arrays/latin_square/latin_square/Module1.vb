868ee87508324b0b59fe40d79abb3c87f8f7ec2f
rafehy bin asif <rafehy6@gmail.com>
2024-09-05 15:23:08 +0500

    complete

---
+++ arrays/latin_square/latin_square/Module1.vb
@@ -0,0 +1,28 @@
+﻿Imports System.Console
+Module Module1
+
+    Sub Main()
+        'Had to get help for this code
+        Dim i, j As Integer
+
+        WriteLine("Enter n for a latin square of size n.")
+        Dim n As Integer = ReadLine()
+        Dim latin_square(n - 1, n - 1) As Integer
+
+        For i = 0 To (n - 1)
+            For j = 0 To (n - 1)
+                latin_square(i, j) = ((i + j) Mod n) + 1
+            Next
+        Next
+
+        For i = 0 To (n - 1)
+            For j = 0 To (n - 1)
+                Console.Write(latin_square(i, j) & " ")
+            Next
+            Console.WriteLine()
+        Next
+
+        ReadKey()
+    End Sub
+
+End Module
---
+++ arrays/latin_square/latin_square/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("latin_square")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("latin_square")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("76d78d33-f96d-4dfe-ba77-432b8f88a433")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
